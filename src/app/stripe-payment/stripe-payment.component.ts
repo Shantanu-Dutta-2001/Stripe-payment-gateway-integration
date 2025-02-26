25b54998b3f4f6b2b923a3cd7b86495e99f9ef5a
ShantanuDutta <shantanudutta07@gmail.com>
2025-02-26 21:21:49 +0530

    setup project structure and components

---
+++ src/app/stripe-payment/stripe-payment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stripe-payment',
+  imports: [],
+  templateUrl: './stripe-payment.component.html',
+  styleUrl: './stripe-payment.component.scss'
+})
+export class StripePaymentComponent {
+
+}
