476d24fe71dca297123615a4949dda9a90e14a69
ShantanuDutta <shantanudutta07@gmail.com>
2025-03-01 18:42:02 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Y
+# StripePaymentApp
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.1.8.
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { StripePaymentComponent } from './Components/stripe-payment/stripe-payment.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: StripePaymentComponent  },
+];
